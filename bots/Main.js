a3794cee6326003a367a5f735d5f8a655917bd07
CranFused <121121675+CranFused-Real@users.noreply.github.com>
2023-02-22 17:51:06 +0530

    Add files via upload

--- bots/Main.js
+++ bots/Main.js
@@ -1,29 +1,27 @@
-// Pretty Simple Bot which will only join Your SinglePlayer LAN world
-
 const mineflayer = require('mineflayer');
+const { pathfinder } = require('mineflayer-pathfinder')
 
-// Connect to the Minecraft server
 const bot = mineflayer.createBot({
-  host: '<Your Private IP>',
-  port: 1234, // Keep the port number same as on which your LAN Game server is running!
-  username: 'Hello_world', // Just enter Any username you want... I guess spaces dont work (IDK)
-  version: '1.18.1' // Version in which your LAN game is connected
+    host: '192.168.x.x', // Set you host's IP Address
+    port: 4444, // Keep the port number same as on which your LAN Game server is running!
+    username: 'CranFused' // Just enter Any username you want... I guess spaces dont work (IDK)
 });
 
-// Wait for the bot to connect to the server
-bot.once('login', () => {
-  console.log('Bot connected to the server!');
+bot.loadPlugin(pathfinder)
 
-  // Disconnect the bot from the server after 10 seconds
-  setTimeout(() => {
-    console.log('Disconnecting from the server...');
-    bot.end();
-  }, 10000);
+// This Function will follow the player
+function followPlayer() {
+    const PlayerCI = bot.players['CranFused_'] // Name of the player which bot will follow
+
+// If player is not in the world then it will give this error
+    if (!playerCI) (
+        bot.chat("I cant see CI!")
+        return
+    )
+}
+
+// Bot will send a message that Bot has joined the Game
+bot.on('spawn', () => {
+    console.log('Bot spawned in the game.');
 });
 
-bot.chat('Hello, world!');
-
-// Handle any errors that occur while connecting to the server
-bot.on('error', (err) => {
-  console.error(`An error occurred: ${err.message}`);
-});
